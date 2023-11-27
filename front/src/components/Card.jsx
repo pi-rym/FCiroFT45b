e7b770a158c894030fd1580067ad56fc5b760afb
Felipe Ciro Montoya <fmontoya@soyhenry.com>
2023-11-27 10:56:06 -0500

    vinculando repo

---
+++ front/src/components/Card.jsx
@@ -0,0 +1,14 @@
+export default function Card(props) {
+  return (
+      <div>
+          <h1>Esta es la Card</h1>
+      {/* <button onClick={}>X</button>
+         <h2></h2>
+         <h2></h2>
+         <h2></h2>
+         <h2></h2>
+         <h2></h2>
+         <img src={} alt='' /> */}
+    </div>
+  );
+}
---
+++ front/src/components/Cards.jsx
@@ -0,0 +1,5 @@
+import Card from "./Card";
+
+export default function Cards(props) {
+  return <div></div>;
+}
